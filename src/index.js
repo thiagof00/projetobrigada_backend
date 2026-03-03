d1d84f0a84f8e6a64e24e2cfa559062025dc34ca
thiagof00 <thiagoribeiro0302@gmail.com>
2026-03-02 22:07:36 -0300

    Migração do better sqlite para prismajs com postegresql em nuvem no supabase

--- src/index.js
+++ src/index.js
@@ -2,32 +2,34 @@ const express = require('express')
 const http = require('http')
 const cors = require('cors')
 const { Server } = require('socket.io')
+require('dotenv').config()
 
+const { initDB } = require('./db/init')
 const { setupPresence } = require('./sockets/presence')
 const ocorrenciasRoutes = require('./routes/ocorrencias.routes')
 
-require('./db/init')
-
 const app = express()
 app.use(cors())
+app.use(express.json())
+
 const server = http.createServer(app)
 const io = new Server(server, {
   cors: { origin: '*' }
 })
 
-app.use(express.json())
-
-// ativa controle de presença
+// Ativa controle de presença
 setupPresence(io)
 
-// usa rotas passando io
+// Rotas
 app.use('/ocorrencias', ocorrenciasRoutes(io))
 app.use('/usuarios', require('./routes/usuarios.routes'))
 app.use('/logs', require('./routes/logs.routes'))
 
+async function main() {
+  await initDB()
+  server.listen(3432, () => {
+    console.log('🚨 API SOS rodando com WebSocket na porta 3432')
+  })
+}
 
-
-
-server.listen(3432, () => {
-  console.log('🚨 API SOS rodando com WebSocket')
-})
+main()
